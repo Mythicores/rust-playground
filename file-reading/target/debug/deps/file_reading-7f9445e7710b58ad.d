ed146f70f2f02049ceae33f68abeff7f97814423
Mythicores <njones3@byupathway.edu>
2026-02-11 21:24:49 -0700

    Added file-reading subproject - secret file copied & reading!

---
+++ file-reading/target/debug/deps/file_reading-7f9445e7710b58ad.d
@@ -0,0 +1,5 @@
+/home/nathanael/personalProjects/rust-playground-guess/file-reading/target/debug/deps/file_reading-7f9445e7710b58ad.d: src/main.rs
+
+/home/nathanael/personalProjects/rust-playground-guess/file-reading/target/debug/deps/file_reading-7f9445e7710b58ad: src/main.rs
+
+src/main.rs:
---
+++ file-reading/target/debug/file-reading.d
@@ -0,0 +1,1 @@
+/home/nathanael/personalProjects/rust-playground-guess/file-reading/target/debug/file-reading: /home/nathanael/personalProjects/rust-playground-guess/file-reading/src/main.rs
