a343c4ed464a4d878e3f89824398f845b4600e27
Mythicores <njones3@byupathway.edu>
2026-03-21 18:25:31 -0600

    v1 rewrite - discovered the issue. Version mismatch. The type of code I was using required version 0.8.5, but I was using 0.10.0. Switched back, and voila!

--- guessing_game_v1/src/main.rs
+++ guessing_game_v1/src/main.rs
@@ -1,15 +1,21 @@
 use std::io;
 use rand::Rng;
-use rand::thread_rng;
+
 
 fn main() {
     println!("\nWelcome to the guessing game!\n");
     println!("Write your guess below!:\n");
-    let mut guess = String::new();
+
+
+    let mut guess = String::new(); //Creating variable for user input
+
+
     io::stdin() //stdin: Under the "io" module. Allows us to interact with the user for input.
         .read_line(&mut guess) //
         .expect("Failed to read line.");
-    let secret_number = thread_rng().gen_range(1..=100);
+
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("\nThe secret number is {secret_number}.");
     
 }
