6f92847ff41e9d897244d1b6934f5515372e0379
Mythicores <njones3@byupathway.edu>
2026-03-26 18:14:34 -0600

    Based purely off memory. Obviously it doesn't run yet. I'll continue tomorrow. The Forge never stops.

--- guessing_game_v1/src/main.rs
+++ guessing_game_v1/src/main.rs
@@ -5,24 +5,36 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("\nWelcome to the guessing game!\n");
-    println!("Write your guess below!:\n");
-
     
-    let mut guess = String::new(); //Creating variable for user input
+    let mut guess_amount = 0;
+    let secret_number = rand::thread_rng().gen_range(1..=100);    
 
+    loop{
+        println!("Write your guess below!:\n"); //Gotta make sure it prompts them for the guess each time.
+        let mut guess = String::new(); //Creating variable for user input
+        io::stdin() //stdin: Under the "io" module. Allows us to interact with the user for input.
+            .read_line(&mut guess) //
+            .expect("Failed to read line.");
 
-    io::stdin() //stdin: Under the "io" module. Allows us to interact with the user for input.
-        .read_line(&mut guess) //
-        .expect("Failed to read line.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {println!("That's not a number, please try again!");
+                continue;
+            }
+        };
+        guess_amount = guess_amount +1;
+        println!("You guessed: {guess}");
+        
+        println!("\nThe secret number is {secret_number}.");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
-    
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("\nThe secret number is {secret_number}.");
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("That's too low!"),
-        Ordering::Greater => println!("That's too high!"),
-        Ordering::Equal => println!("You got it!"),
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("That's too low!"),
+            Ordering::Greater => println!("That's too high!"),
+            Ordering::Equal => { 
+                println!("You got it!\n\nThanks for playing!\n\nYou guessed {guess_amount} times!");
+                break;
+            }
+        }
     }
     
 }
